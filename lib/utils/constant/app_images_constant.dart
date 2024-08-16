5f1bc00b182f73c6df6e4b78d93880cb93073329
Kashan Ashraf <106448203+Kashannn@users.noreply.github.com>
2024-08-16 11:47:25 +0500

    UI done

---
+++ lib/utils/constant/app_images_constant.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppImages extends AssetImage {
+  const AppImages(super.assetName);
+  static const String _assetsPath = 'assets/Images';
+  static const String _iconPath = 'assets/IconsSvg';
+//PNG images
+  static const String kashan = '$_assetsPath/kashan.jpg';
+  static const String Baby = '$_assetsPath/Baby.png';
+  static const String Hoodie = '$_assetsPath/Hoodie.png';
+  static const String Jacket = '$_assetsPath/Jacket.png';
+  static const String shoes = '$_assetsPath/shoes.png';
+  static const String TV = '$_assetsPath/TV.png';
+  static const String watch = '$_assetsPath/watch.png';
+  static const String airpods = '$_assetsPath/airpods.png';
+
+
+//Svg images
+  static const String uploadLogo = '$_iconPath/UploadLogo.svg';
+
+}
